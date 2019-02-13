98e80b5c4fa68ccff5bee5dba2e9e1d732b09629
Marlon Hildon Junior <marlonhildon@gmail.com>
2019-02-13 17:46:11 -0300

    Adicionada Classe Department

---
+++ src/main/java/com/hildon/marlon/application/Program.java
@@ -0,0 +1,13 @@
+package com.hildon.marlon.application;
+
+import com.hildon.marlon.model.entities.Department;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Department obj = new Department(1, "Books");
+		System.out.println(obj);
+	}
+
+}
